70dc25cbdc27f3b559526c74c031accbf2d61dee
GuillaumeCa <guillaume.carre@isep.fr>
2015-10-06 00:32:00 +0200

    correction

--- src/table_multiplication2.java
+++ src/table_multiplication2.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class table_multiplication2 {
 	
-	public static int[][] calculerTable(int nb, int max) {
-		int tab2 [][] = new int[max+1][max+1];
+	public static int[] calculerTable(int nb, int max) {
+		int tab2 [] = new int[max+1];
 		for (int k=0; k <= max; k++) {
-			for (int i=0; i <= max; i++) {
-				tab2[k][i] = k*i;
-			}
+			tab2[k]= nb*k;
 		}
 		return tab2;
 	}
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Nombre départ ? ");
+		System.out.print("Nombre ? ");
 		int nb = scan.nextInt();
-		System.out.print("Nombre fin ? ");
+		System.out.print("Nombre max ? ");
 		int max = scan.nextInt();
-		int[][] tab = calculerTable(nb,max);
+		int[] tab = calculerTable(nb,max);
+		System.out.println(Arrays.toString(tab));
 	}
 }
